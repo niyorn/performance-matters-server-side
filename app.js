7691e5ed66ae44a23b2e567125ff4a84a6379d30
Fenna <fennadewilde@hotmail.com>
2018-03-20 12:34:26 +0100

    added a .env and moved api server side, doesnt work

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const sassMiddleware = require('node-sass-middleware');
-const vision = require('@google-cloud/vision');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
 const api = require('./routes/api');
+require('dotenv').config({ path: './vars.env'})
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -52,6 +49,6 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(8000, () => console.log('Example app listening on port 8000!'));
 
 module.exports = app;
